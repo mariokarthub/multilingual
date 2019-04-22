da7cae5832520da4ef2d0d0ac7884ab0e77b644b
Paul Guillamon <Qu4k3@users.noreply.github.com>
2019-04-22 14:14:14 +0200

    Create general.php

---
+++ src/es/general.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation Menu Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the menu displayed at the top.
+    |
+    */
+    
+    'home' => 'Inicio',
+    'teams'     => 'Equipos',
+];
